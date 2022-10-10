9538ba18227ab9b8570c4a8e072eec6bb9bfb008
Kassidy <dragonlinkx@gmail.com>
2022-10-10 10:34:09 -0600

    Added ADT gonna see if hastab is an ADT or not

--- hash.c
+++ hash.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
+struct linkedlist
+{
+        long long key;
+        char *word;
+        struct linkedlist *next;
+};
+
+
+struct hashtab
+{
+        long long key;
+        struct linkedlist *val;
+        struct hashtab *next;
+};
+
+
 int main()
 {
 
