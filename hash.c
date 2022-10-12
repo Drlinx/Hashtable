8483a5a9f4b3602ee9f9c12e942a975755462507
Kassidy <dragonlinkx@gmail.com>
2022-10-12 13:50:46 -0600

    added initalization stuff

--- hash.c
+++ hash.c
@@ -15,7 +15,7 @@ struct linkedlist
 
 struct hashtab
 {
-        struct linkedlist bucket[31];
+        struct linkedlist *bucket;
 };
 
 void createtable(int len, struct linkedlist *hash);
@@ -24,16 +24,36 @@ long long pow(long long base, int power);
 
 int main()
 {
-        struct linkedlist hash[31];
+        //Hashtab works as an array of linked lists which represent buckets.
+        struct hashtab hash[31];
 
 
         return 0;
 }
 
+/**
+ * @brief quickly sets up an array of buckets by setting each pointer to null.
+ * 
+ * @param hash the hashtable itself.
+ * @param len the length of said hashtable. 
+ */
+void inithas(struct hashtab *hash, int len)
+{
+        for(int i = 0; i < len; i++){
+                hash[i].bucket = NULL;
+        }
+}
+
 
 struct linkedlist *initialize(char *mess)
 {
-
+        struct linkedlist *new = malloc(sizeof(struct linkedlist));
+        if(!new)
+                exit(6);
+        new->word = mess;
+        new->key = radix(mess);
+        new->next = NULL;
+        return new;
 }
 
 
