f3277749dc92b1a1912ce26d72fb0a015564f266
Kassidy <dragonlinkx@gmail.com>
2022-10-12 11:28:31 -0600

    added helper functions

--- hash.c
+++ hash.c
@@ -13,20 +13,57 @@ struct linkedlist
         struct linkedlist *next;
 };
 
-
 struct hashtab
 {
-        long long key;
-        struct linkedlist *val;
-        struct hashtab *next;
+        struct linkedlist bucket[31];
 };
 
-
+void createtable(int len, struct linkedlist *hash);
 
 int main()
 {
-
+        struct linkedlist hash[31];
+        for(int i = 0; i < 31; i++)
+                printf("%c", hash[i].real);
 
 
         return 0;
+}
+
+
+void createtable(int len, struct linkedlist *hash)
+{
+        for (int i = 0; i < len; i++){
+                hash[i].real = 'F';
+                hash[i].next = NULL;
+        }
+
+}
+
+
+struct linkedlist *initialize(char *mess)
+{
+
+}
+
+
+int radix(char *mess)
+{
+        int key;
+
+
+        return key;
+}
+
+
+int pow(int base, int power)
+{
+        if(power == 0){
+                base = 0;
+        } else{
+                for (int i = 0; i < power; i++)
+                        base *= base;
+        }
+
+        return base;
 }
